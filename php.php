48f0ebac8999949fb78c7c11818884deb8628c95
filippo-bilardo <filippo.bilardo@tiscali.it>
2024-02-16 17:14:04 +0000

    update

--- php.php
+++ php.php
@@ -1,3 +1,14 @@
-<?php
-phpinfo();
-?>
+<html>
+<head>
+    <title>Hello php</title>
+</head>
+<body>
+
+    <h1>Hello World!</h1>
+    <br />
+    <?php
+    phpinfo();
+    ?> 
+
+</body>
+</html>
